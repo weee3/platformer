14dd08429cd31d26e3efe4f4fbfed3160bfa868d
weee3 <99814383+weee3@users.noreply.github.com>
2022-02-17 14:53:17 -0500

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (jumpcount <= 1) {
+        player.vy = -150
+        jumpcount += 1
+    } else if (jumpcount == 2) {
+        player.vy = -100
+        jumpcount += 1
+    }
     if (player.isHittingTile(CollisionDirection.Bottom)) {
-        player.vy = -170
+        jumpcount = 0
     }
 })
 function replaceTile (tile: Image, image2: Image, kind: number) {
@@ -10,8 +17,13 @@ function replaceTile (tile: Image, image2: Image, kind: number) {
         tiles.placeOnTile(sprtie, t)
     }
 }
+// animation.runImageAnimation(
+// firesprite,
+// assets.animation`fire`,
+// 200,
+// true
+// )
 function startlevel (level: number) {
-    let firesprite: Sprite = null
     tiles.setTilemap(tilemap`level1`)
     replaceTile(assets.tile`flag`, flagImage, 2)
     replaceTile(assets.tile`fire1`, fireimage, 3)
@@ -21,32 +33,25 @@ function startlevel (level: number) {
     player.x = levels[level].startx * 16
     player.y = levels[level].starty * 16
     controller.moveSprite(player, 120, 0)
- 
-    animation.runImageAnimation(
-    firesprite,
-    assets.animation`fire`,
-    200,
-    true
-    )
-
 }
-
 let sprtie: Sprite = null
 let flagImage: Image = null
 let playersprite: Image = null
+let jumpcount = 0
+let firesprite = null
 let fireimage: Image = null
 let doublejumpcounter = 0
-let enemy: Sprite = null
+let enemy = null
 let player: Sprite = null
-if (true) 
-	
-
-function firedamage (sprite=player ,overlapsWith = fireimage) {
+if (true) {
+    function firedamage (sprite=player ,overlapsWith = fireimage) {
      info.player1.changeLifeBy(-1)
      pause (100)
 }
+}
 scene.setBackgroundImage(assets.image`night backround`)
 let playerspritejump : Image
+jumpcount = 0
 let levels = [{
     tilemap: tilemap`level1`,
     startx: 1,
@@ -58,23 +63,23 @@ let levels = [{
     }]
 playersprite = assets.image`player`
 flagImage = img`
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . f 2 . . . . . . .
-    . . . . . . . f 2 2 2 2 . . . .
-    . . . . . . . f 2 2 2 2 2 2 . .
-    . . . . . . . f 2 2 2 2 2 2 2 .
-    . . . . . . . f 2 2 2 2 2 2 . .
-    . . . . . . . f 2 2 2 2 . . . .
-    . . . . . . . f 2 . . . . . . .
-    . . . . . . . f . . . . . . . .
-    . . . . . . . f . . . . . . . .
-    . . . . . . . f . . . . . . . .
-    . . . . . . . f . . . . . . . .
-    . . . . . . . f . . . . . . . .
-    . . . . . . . f . . . . . . . .
-`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . f 2 . . . . . . . 
+    . . . . . . . f 2 2 2 2 . . . . 
+    . . . . . . . f 2 2 2 2 2 2 . . 
+    . . . . . . . f 2 2 2 2 2 2 2 . 
+    . . . . . . . f 2 2 2 2 2 2 . . 
+    . . . . . . . f 2 2 2 2 . . . . 
+    . . . . . . . f 2 . . . . . . . 
+    . . . . . . . f . . . . . . . . 
+    . . . . . . . f . . . . . . . . 
+    . . . . . . . f . . . . . . . . 
+    . . . . . . . f . . . . . . . . 
+    . . . . . . . f . . . . . . . . 
+    . . . . . . . f . . . . . . . . 
+    `
 fireimage = assets.image`fire frame 1`
 playerspritejump = assets.image`player-jump`
 let enemyimage = assets.image`enemy1`
